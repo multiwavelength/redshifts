331d8f2e8b47b9e74d0d96247224e8a09fce21e7
Andra Stroe <andrastroe1987@gmail.com>
2020-01-29 15:57:49 -0500

    Remove sound at the end

--- PIPELINE_online_redshift.py
+++ PIPELINE_online_redshift.py
@@ -5,7 +5,7 @@ from astropy.table import QTable
 import astropy.coordinates as coord
 
 import query as q
- 
+
 # Set the paths where the data will be downloaded
 base_path = f'/home/andra/Desktop/Keep/astroquery'
 data_path = f'{base_path}/clusters'
@@ -18,15 +18,13 @@ for cluster in cluster_table:
     # Set the base name/identifier for the source around which redshifts will
     # be searched for 
     name = cluster['Cluster']
-    
+
     if not os.path.exists(f'{data_path}/{name}'):
         os.mkdir(f'{data_path}/{name}')
     
     # Set the central coordinates; in this case they are taken from the cluster
     # table as the RA and DEC of the cluster, but this can be set to anything
     coords = coord.SkyCoord(cluster['RA'], cluster['DEC'])
-    q.run_query(data_path, name, coords)
 
-duration = 1  # seconds
-freq = 440.  # Hz
-os.system('play -nq -t alsa synth {} sine {}'.format(duration, freq))
+    # Run the query
+    q.run_query(data_path, name, coords)
