4f2adcc98bf679c623bf1b49a22ff67d365a3153
Andra Stroe <andrastroe1987@gmail.com>
2020-01-29 14:50:41 -0500

    Wrapper to run the package for each target

--- PIPELINE_online_redshift.py
+++ PIPELINE_online_redshift.py
@@ -11,19 +11,45 @@ import query as q
 from astroquery.vizier import Vizier
 import cross_match_utilities as cmu
 
-for cluster in cluster_table[0:1]:
+# Set the paths where the data will be downloaded
+base_path = f'/home/andra/Desktop/Keep/astroquery'
+data_path = f'{base_path}/clusters'
+pipeline_path = f'{base_path}/pipeline'
+
+# A fits file containing all the sources we want to download redshifts for
+cluster_table = QTable.read(f'{data_path}/Cluster_properties.fits')
+
+for cluster in cluster_table:
+    # Set the base name/identifier for the source around which redshifts will
+    # be searched for 
+    name = cluster['Cluster']
+    
+    if not os.path.exists(f'{data_path}/{name}'):
+        os.mkdir(f'{data_path}/{name}')
+    
+    # Set the central coordinates; in this case they are taken from the cluster
+    # table as the RA and DEC of the cluster, but this can be set to anything
     coords = coord.SkyCoord(cluster['RA'], cluster['DEC'])
     
-    gt = q.query_vizier(coords, 'redshift')
-    gt.write('grand_Redshift.fits', format='fits', overwrite=True)
+    # Set the paths based on the where you want the data to be downloaded and 
+    # the identified for the sources/field the redshifts are downloaded for    
+    path_concat = f'{data_path}/{name}/{name}_online_redshift.fits'
+    path_ident = f'{path_concat.replace(".fits", "")}_ident.fits'
+    path_unique = f'{path_concat.replace(".fits", "")}_ident_unique.fits'
 
-    gt = q.query_vizier(coords, 'velocity')
-    gt.write('grand_Redshift_vel.fits', format='fits', overwrite=True)
+    # Perform the redshift query on Vizier and NED and write to fits file
+    grand_table = q.query_redshift(coords, data_path, name)
+    grand_table.meta['description'] = u'Vizier and NED redshifts'
+    grand_table.write(path_concat, format='fits', overwrite=True)
 
-    gt = q.query_NED(coords)
-    gt.write('grand_Redshift_NED.fits', format='fits', overwrite=True)
-
-
+    # Identify duplicates and keep only the best redshift measurement
+    duplicates = cmu.identify_duplicates(path_concat, path_ident, 
+                                         RA='RA', DEC='DEC')
+    if duplicates==True:
+        cmu.find_groups_redshift(path_ident, path_unique)
+    else:
+        shutil.copyfile(path_ident, path_unique)
+    
 import os
 duration = 1  # seconds
 freq = 440.  # Hz
