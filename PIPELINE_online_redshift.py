1893b39db26ba4a6525012bf4b5f7b56a3e91569
Andra Stroe <andrastroe1987@gmail.com>
2020-05-26 20:25:50 -0400

    Read in param from click and from config file

--- PIPELINE_online_redshift.py
+++ PIPELINE_online_redshift.py
@@ -1,30 +1,29 @@
 import os
 import warnings
 warnings.filterwarnings("ignore")
-from astropy.table import QTable
-import astropy.coordinates as coord
+import click
 
 import query as q
+import constants as c
 
-# Set the paths where the data will be downloaded
-base_path = f'/home/andra/Desktop/Keep/astroquery'
-data_path = f'{base_path}/clusters'
-pipeline_path = f'{base_path}/pipeline'
-
-# A fits file containing all the sources we want to download redshifts for
-cluster_table = QTable.read(f'{data_path}/Cluster_properties.fits')
-
-for cluster in cluster_table:
-    # Set the base name/identifier for the source around which redshifts will
-    # be searched for 
-    name = cluster['Cluster']
-
-    if not os.path.exists(f'{data_path}/{name}'):
-        os.mkdir(f'{data_path}/{name}')
+def redshifts(path, name, RA, DEC, config_path):    
+    # Read in configuration
+    config = c.read_config(config_path)
     
-    # Set the central coordinates; in this case they are taken from the cluster
-    # table as the RA and DEC of the cluster, but this can be set to anything
-    coords = coord.SkyCoord(cluster['RA'], cluster['DEC'])
-
     # Run the query
-    q.run_query(data_path, name, coords)
+    q.run_query(path, name, RA, DEC, config)
+
+
+@click.command()
+@click.option("--path", default='.', help="Path for the data")
+@click.option("--config", default="redshiftconfig.yaml", help='Configuration file in YAML format.')
+@click.option("--name", default="", help='Target name.')
+@click.option("--RA", default="", help='Right ascension, with units: e.g. 150d, 150deg, 12h')
+@click.option("--DEC", default="", help='Declination, with units: e.g. 30d, 30deg')
+def main(path, config, name, ra, dec):  
+    # A fits file containing all the sources we want to download redshifts for
+    redshifts(path, name, ra, dec, config)
+
+
+if __name__ == "__main__":
+    main()
